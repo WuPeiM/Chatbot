e07ee5ab43a537ae18a4919ebdba957659af3d5d
吴培铭 <1780439574@qq.com>
2023-11-30 17:27:02 +0800

    feature: 新增输入框和按钮

--- chatbot/views/story/index.vue
+++ chatbot/views/story/index.vue
@@ -2,6 +2,18 @@
     <div class="story-page">
         <page-explanation :title="'故事'" :content="'故事是一系列有序的意图和响应组合，用于训练模型以理解并回应用户的不同交互场景'"></page-explanation>
         <div class="story-box">
+            <div class="head">
+                <bk-input
+                    class="search-box"
+                    :clearable="true"
+                    :placeholder="'请输入名称进行搜索'"
+                    :right-icon="'bk-icon icon-search'"
+                    v-model="searchName"
+                    @right-icon-click="searchStory"
+                    @enter="searchStory">
+                </bk-input>
+                <bk-button class="btn" theme="primary" title="创建" icon="icon-bk" @click="handleAdd">创建故事</bk-button>
+            </div>
         </div>
     </div>
 </template>
@@ -16,13 +28,31 @@
     }
 })
 
-export default class Intention extends Vue {}
+export default class Intention extends Vue {
+    searchName: string = ''
+
+    searchStory() {
+        console.log('搜索', this.searchName)
+    }
+    handleAdd() {
+        console.log('新增')
+    }
+}
 </script>
 
 <style  lang="scss" scoped>
 .story-page {
     .story-box {
-        display: flex;
+        background-color: #fff;
+        min-height: calc(100vh - 200px);
+        padding: 20px 15px;
+        .head {
+            display: flex;
+            justify-content: space-between;
+        }
+        .search-box {
+            width: 400px;
+        }
     }
 }
 </style>
