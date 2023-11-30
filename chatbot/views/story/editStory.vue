e211fe5a9b63fd42dde4e911932bdb188d1cf4d0
吴培铭 <1780439574@qq.com>
2023-11-30 16:50:04 +0800

    feature: 新增编辑故事页面

---
+++ chatbot/views/story/editStory.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="story-edit">
+        <div class="story-box">
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+    components: {}
+})
+
+export default class Intention extends Vue {}
+</script>
+
+<style  lang="scss" scoped>
+.story-edit {
+    .story-box {
+        display: flex;
+    }
+}
+</style>
